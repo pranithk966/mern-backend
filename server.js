cf3d19a32f4f3259309cfa8405ea30d7722d64d0
pranith <pranithk966@gmail.com>
2024-05-16 12:06:15 +0530

    main and last

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// app.get('/', (req, res) => {
-//   res.json({ mssg: 'home page GET' })
-// })
+app.get('/', (req, res) => {
+  res.json({ mssg: 'home page GET' })
+})
 
 app.use('/tasks', router)
 
